14be9617f468a3f9c24248b47a4e4008d000cc4f
Ponkrit <ponkrit01@hotmail.com>
2020-04-13 14:53:25 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,7 +2,21 @@
 <html lang="en">
 <body>
     <?php
+
     echo "My second page PHP Script";
+
+    $x = 5985;
+    var_dump(is_numeric($x));
+
+    $x = "5985";
+    var_dump(is_numeric($x));
+
+    $x = "59.85" - 100;
+    var_dump(is_numeric($x));
+
+    $x = "Hello";
+    var_dump(is_numeric($x));
+
     ?>
 </body>
 </html>
