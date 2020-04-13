09a0f0a6ce08bd71acb584ad1d0117d24a99f23e
Ponkrit <ponkrit01@hotmail.com>
2020-04-13 15:09:20 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,11 +2,14 @@
 <html>
 <body>
 <?php
-echo "My secound page PHP script!";
-$x = 15985;
-var_dump(is_int($x));
-$x = 3359.85;
-var_dump(is_int($x));
+    echo "My secound page PHP script!";
+
+    $x = 5985;
+    var_dump(is_int($x));
+
+    $x = 159.85;
+    var_dump(is_int($x));
+
 ?>
 </body>
 </html>
